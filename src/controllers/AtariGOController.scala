1019a2901d2624b696410338a6746948e9836e61
darloETIES <darlo@iscte-iul.pt>
2025-05-12 19:13:03 +0100

    Continuação da implementação da GUI do tabuleiro e opções

--- src/controllers/AtariGOController.scala
+++ src/controllers/AtariGOController.scala
@@ -1,5 +1,38 @@
 package controllers
 
+import javafx.fxml.FXML
+import javafx.scene.control.{Button, Slider}
+import javafx.scene.layout.{BorderPane, ColumnConstraints, GridPane, Pane, RowConstraints, StackPane}
+
 class AtariGOController {
 
+  @FXML
+  private var gridPaneGame:GridPane = _
+
+
+  //gameOptions widgets
+  @FXML
+  private var borderPaneOptions:BorderPane = _
+
+  @FXML
+  private var playButton:Button = _
+
+  //reinicia o jogo, limpando o tabuleiro
+  def resetGame(): Unit={
+    gridPaneGame.getChildren.clear()
+    gridPaneGame.getColumnConstraints.clear()
+    gridPaneGame.getRowConstraints.clear()
+  }
+
+
+  def onPlayButtonClicked(): Unit = {
+    if(gridPaneGame != null) resetGame()
+
+    //fecha a janela modal das opcoes
+    val parent = borderPaneOptions.getParent //irá obter o StackPane
+    if(parent != null && parent.isInstanceOf[Pane]){
+      val pane = parent.asInstanceOf[Pane]
+      pane.getChildren.remove(borderPaneOptions)
+    }
+  }
 }
--- src/controllers/ChooseViewController.scala
+++ src/controllers/ChooseViewController.scala
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent
 import javafx.fxml.{FXML, FXMLLoader}
 import javafx.scene.{Node, Parent, Scene}
 import javafx.scene.control.Button
+import javafx.scene.layout.StackPane
 import javafx.stage.Stage
 
 class ChooseViewController {
@@ -15,14 +16,23 @@ class ChooseViewController {
   private var buttonTUI: Button = _
 
   def onButtonGUIClicked(): Unit={
+
+    //carrega a Scene com uma Scene modal embutida (2 Scenes numa Stage)
     val AtariGOStage: Stage = new Stage()
-    val fxmlLoader = new FXMLLoader(getClass.getResource("/views/AtariGoView.fxml"))
+    val fxmlLoader = new FXMLLoader(getClass.getResource("/views/atariGoView.fxml"))
     val mainViewRoot: Parent = fxmlLoader.load()
     val scene = new Scene(mainViewRoot)
     AtariGOStage.setScene(scene)
     AtariGOStage.show()
 
-    //Fecha a janela atual
+    //Fecha a janela atual (chooseView)
     buttonGUI.getScene.getWindow.hide()
+
+    //carrega a janela modal das opcoes do jogo
+    val gameOptionsModalLoader = new FXMLLoader(getClass.getResource("/views/gameOptionsView.fxml"))
+    val modalContent:Parent = gameOptionsModalLoader.load()
+
+    val rootPane = mainViewRoot.asInstanceOf[StackPane]
+    rootPane.getChildren.add(modalContent)
   }
 }
