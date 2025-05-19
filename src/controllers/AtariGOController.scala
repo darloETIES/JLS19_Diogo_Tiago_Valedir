f712438d74e9ecc88027c38d78cd109e7495703e
darloETIES <darlo@iscte-iul.pt>
2025-05-19 18:18:26 +0100

    Temporizador de jogada na GUI
    Implementação da funcionalidade básica do temporizador de jogada na GUI (puramente GUI, não terá lógica de jogo)

--- src/controllers/AtariGOController.scala
+++ src/controllers/AtariGOController.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import com.sun.glass.ui.Application.EventHandler
+import javafx.animation.{Animation, FadeTransition, PauseTransition}
 import javafx.fxml.{FXML, FXMLLoader}
 import javafx.scene.{Parent, Scene}
 import javafx.scene.control.{Button, Label, ProgressBar, Slider}
@@ -9,6 +10,7 @@ import javafx.scene.image.{Image, ImageView}
 import javafx.scene.layout.{BorderPane, ColumnConstraints, GridPane, Pane, RowConstraints, StackPane}
 import javafx.scene.paint.Color
 import javafx.stage.{Modality, Stage}
+import javafx.util.Duration
 
 class AtariGOController {
 
@@ -38,11 +40,13 @@ class AtariGOController {
   private var optionsViewController: OptionsViewController = _
 
 
+  //começa o jogo, com o clicar do botão de Play do menu das opcoes
   def startGameWithColor(color: String):Unit = {
 
     //cria a interatividade com as celulas da grid, considerando a cor do jogador
     addClickListernersToGrid(gridPaneGame.getChildren, color)
     println(color)
+    moveTimer(30)
   }
 
   def addClickListernersToGrid(children: java.util.List[javafx.scene.Node],playerColor:String, i: Int = 0): Unit = {
@@ -98,6 +102,34 @@ class AtariGOController {
       addClickListernersToGrid(children, playerColor, i+1)
 
     }
+  }
+
+  def moveTimer(turnTime:Int): Unit = {
+    val step = 1.0 / turnTime
+
+    def stepBar(current:Int): Unit = {
+      if(current > turnTime) return //mudar para depois passar a vez ao próximo jogador
+
+      val timeRemaining = turnTime - current
+      turnTimeLabel.setText(timeRemaining.toString)
+
+      if(timeRemaining <= 10){
+        turnTimeLabel.setTextFill(Color.color(1,0,0))
+        turnTimeProgressBar.setStyle("-fx-accent: red;")
+      }
+
+      turnTimeProgressBar.setProgress(current * step)
+      //pois considerando que será de 1 segundo em 1 segundo, o valor atual multiplicado com o avanço dará o progresso da barra
+      //está fora do evento abaixo pois será necessário atualizar logo imediatamente o estado atual da barra
+
+      val pauseTransition = new PauseTransition(Duration.seconds(1)) //cria o temporizador que vai esperar 1 segundo
+      pauseTransition.setOnFinished(_ => { //executa todo o código abaixo durante 1 segundo que foi definido
+
+        stepBar(current+1) //assim incrementa 1 no current, para que no inicio dê 0 e nos restantes vá subido como suposto
+      })
+      pauseTransition.play() //inicia o temporizador com 1 segundo
+    }
+    stepBar(0) //começar o progresso em 0
   }
 
   def onNewGameMouseClicked(): Unit = {
